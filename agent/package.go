09452fcb1de884428d1e565be76c44bcfd8c3136
advanced-go <mark.wilson.go@outlook.com>
2025-03-16 10:38:47 -0500

    Updating

--- agent/package.go
+++ agent/package.go
@@ -2,9 +2,9 @@ package agent
 
 import (
 	"github.com/behavioral-ai/caseofficer/agent1"
-	"github.com/behavioral-ai/collective/content"
 	"github.com/behavioral-ai/core/messaging"
 	"github.com/behavioral-ai/domain/common"
+	"github.com/behavioral-ai/domain/content"
 )
 
 func New(origin common.Origin, resolver content.Resolution, dispatcher messaging.Dispatcher) messaging.Agent {
